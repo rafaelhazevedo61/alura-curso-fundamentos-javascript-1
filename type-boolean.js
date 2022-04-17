afb7c010b76ad16320aa8ce595294b64202eff5c
rafael <rafaelhazevedo61@gmail.com>
2022-04-17 00:57:55 -0300

    aula 1 - completa - tipo boolean

---
+++ type-boolean.js
@@ -0,0 +1,19 @@
+//boolean
+//--------------------------------------------
+//true -> verdadeiro
+//false -> falso
+//--------------------------------------------
+//1 simbolo de igual -> atribuindo um valor
+//2 e 3 simbolos de igual -> comparacoes
+//--------------------------------------------
+
+const primeiroNumero = 10;
+const segundoNumero = 10;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
