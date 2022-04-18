388de6760c3d24c6cb20fcf4c92335d615ccb7ba
rafael <rafaelhazevedo61@gmail.com>
2022-04-17 23:54:40 -0300

    aula 2 - parcial - truthy e falsy

---
+++ truthyfalsy.js
@@ -0,0 +1,31 @@
+//boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+//truthy ou falsy
+// 0 -> false
+// 1 -> true
+
+console.log("---------------");
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+console.log("---------------");
+
+//null
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+console.log("---------------");
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof minhaVar);
+console.log(typeof varNull);
