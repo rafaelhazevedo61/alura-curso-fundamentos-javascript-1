fe9109593bd8d5db9fe554e2c01ab1b8b2b0949e
rafael <rafaelhazevedo61@gmail.com>
2022-04-18 00:13:18 -0300

    aula 2 - completa - conversao de tipos

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString); //false
+console.log(numero == numeroString);  //true
+
+console.log(numero + numeroString)
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+
+// conversao explicita
