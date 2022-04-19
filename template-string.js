227d96e50ec1c1d4ad8b68398a3dafa585391015
rafael <rafaelhazevedo61@gmail.com>
2022-04-18 23:48:58 -0300

    aula 4 - completa - template literal

---
+++ template-string.js
@@ -0,0 +1,19 @@
+const nome = "Ju";
+const idade = 2021 - 1981
+const cidadeDeNascimento = "São Paulo"
+
+const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + 
+" e nasci na cidade de " + cidadeDeNascimento
+
+const apresentacao2 = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento} `
+
+const apresentacao3 = `meu nome é ${nome}, minha idade é ${idade} 
+e nasci na cidade de ${cidadeDeNascimento} `
+
+console.log("----------------------------------")
+console.log(apresentacao)
+console.log("----------------------------------")
+console.log(apresentacao2)
+console.log("----------------------------------")
+console.log(apresentacao3)
+console.log("----------------------------------")
