8cf9eb98033002164b6b93e92a41d0ff597cd9f5
rafael <rafaelhazevedo61@gmail.com>
2022-04-19 00:44:21 -0300

    aula 5 - completa - arrow function

---
+++ arrowfunction.js
@@ -0,0 +1,21 @@
+ function apresentar(nome){
+     return `meu nome é ${nome}`
+ }
+
+ const apresentarArrow = nome => `meu nome é ${nome}`
+ const soma = (num1, num2) => num1 + num2;
+
+ console.log(apresentarArrow("rafael"))
+ console.log(soma(1,2))
+
+ //Arrow function com + de 1 linha de instrução
+
+ const somaNumerosPequenos = (num1, num2) => {
+     if (num1 || num2 > 10) {
+         return "somente números e 1 a 9"
+     } else {
+         return num1 + num2;
+     }
+ }
+
+ //Hoisting: arrow function se comporta como expressão
