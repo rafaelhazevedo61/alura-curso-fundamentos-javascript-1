688e1880a13af82337018f95b8cd9adb76fb917c
rafael <rafaelhazevedo61@gmail.com>
2022-04-19 00:08:54 -0300

    aula 5 - parcial - funcoes

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+/* let x = "";
+console.log(x);
+x = "oi"; */
+
+// DECLARACAO DE FUNCAO
+
+// 1) declara a funcao
+                    //string
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+/* imprimeTexto("oi")
+imprimeTexto("outro texto") */
+imprimeTexto(soma())
+
+// tres formas de escrever funcoes
+
+function soma(){
+    return 2 + 2;
+}
+
+/* console.log(soma()) */
